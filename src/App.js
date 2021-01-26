ec60a39e6f026fc9caf38d4c59d0207af960f674
zamanshahed <shaheduz.zaman@northsouth.edu>
2021-01-26 16:41:45 +0600

    function based component done!

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Header from './components/Header'
+
 function App() {
   const name = 'Sumon'
   const flag = 0
@@ -5,6 +7,7 @@ function App() {
     <div className="container">
       <h1>Hello {name}, from REACT {15.3+2} (APP.js)</h1>
       <h2>{flag? "Green Light": "Red Light"}</h2>
+      <Header/>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className="container">
+            <h1>Hello from React Component</h1>
+        </header>
+    )
+}
+
+export default Header

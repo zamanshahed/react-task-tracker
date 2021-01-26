3acd1d33b7cf9e79f2397624e42b66701b2d1806
zamanshahed <shaheduz.zaman@northsouth.edu>
2021-01-26 18:23:50 +0600

    styling using in line css as well as variable declared styling

--- src/App.js
+++ src/App.js
@@ -3,23 +3,23 @@ import Header from './components/Header'
 
 //function based :
 //------------------
-// function App() {
-//   const name = 'Sumon'
-//   const flag = 0
-//   return (
-//     <div className="container">
-//       <h1>Hello {name}, from REACT {15.3+2} (APP.js)</h1>
-//       <h2>{flag? "Green Light": "Red Light"}</h2>
-//       <Header/>
-//     </div>
-//   );
-// }
+function App() {
+  const name = 'Sumon'
+  const flag = 0
+  return (
+    <div className="container">
+      {/* <h1>Hello {name}, from REACT {15.3+2} (APP.js)</h1> */}
+      {/* <h2>{flag? "Green Light": "Red Light"}</h2> */}
+      <Header />
+    </div>
+  );
+}
 
 //class based:
 //------------------
-class App extends React.Component{
-  render(){
-    return <h1>Hello from React Class!</h1>
-  }
-}
+// class App extends React.Component{
+//   render(){
+//     return <h1>Hello from React Class!</h1>
+//   }
+// }
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Header = () => {
+
+const Header = (props) => {
     return (
-        <header className="container">
-            <h1>Hello from React Component</h1>
+        <header>
+            <h1 >Hello {props.title}, from React Component</h1>
         </header>
     )
+}
+
+Header.defaultProps = {
+    title: 'user',
+}
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+const styleSheet = {
+    color:'blue', 
+    backgroundColor: 'black',
 }
 
 export default Header

12481aefba82375609f44708b4de14174730e767
zamanshahed <shaheduz.zaman@northsouth.edu>
2021-02-13 13:22:40 +0600

    add and delete synced with backend

--- src/App.js
+++ src/App.js
@@ -30,16 +30,29 @@ const fetchTasks = async () => {
 
 
 //add task
-const addTask = (task) => {
-  const id = Math.floor(Math.random()*10000)+11
-  console.log(task)
-  console.log(id)
-  const newTask = {id, ...task}
-  setTasks([...tasks, newTask])
+const addTask = async (task) => {
+  const res = await fetch('http://localhost:8000/tasks', {method: 'POST', headers:{
+    'Content-type':'application/json'
+    },
+    body: JSON.stringify(task)
+})
+  
+  const data = await res.json()
+  setTasks([...tasks, data])
+
+  // const id = Math.floor(Math.random()*10000)+11
+  // console.log(task)
+  // console.log(id)
+  // const newTask = {id, ...task}
+  // setTasks([...tasks, newTask])
+
+
 }
 
 //delete task
-const deleteTask = (id) => {
+const deleteTask = async (id) => {
+  await fetch(`http://localhost:8000/tasks/${id}`, {method:'DELETE'})
+
   setTasks(tasks.filter((task)=> task.id !== id))
 }
 

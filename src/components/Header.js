f92b7a770fd40f444ba4ea2fda2022645133e22b
zamanshahed <shaheduz.zaman@northsouth.edu>
2021-02-24 16:50:40 +0600

    about page: add button logically disabled but only shows in homepage using useLocation from react-router-dom

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,15 @@
-import React from 'react'
 import PropTypes from 'prop-types'
+import { useLocation } from 'react-router-dom'
 import Button from './Button'
 
 
 const Header = ({title, onAdd, showAdd}) => {
+    const location = useLocation()
 
     return (
         <header className='header'>
             <h1 >Hello {title}, from React Component</h1>            
-            <Button color= {showAdd ? 'red' : 'Green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
+            {location.pathname === '/' && <Button color= {showAdd ? 'red' : 'Green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>}
             
         </header>
     )
